b2851c8e995cbce55fd067b1215b32081cf042e9
sky-xsk <2530485432@qq.com>
2018-05-21 16:24:40 +0800

    新增accessToken验证和一键复制剪切板功能

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ App({
   },
   globalData: {
      urlBase: "https://api.douban.com",
+     accessToken:"d2b42e8f-0207-4bd5-a3f1-fa8d1c37e4d6",
   },
  
 })
--- pages/first/first.js
+++ pages/first/first.js
@@ -1,19 +1,59 @@
 //index.js
 //获取应用实例
-
 const app = getApp()
 Page({
   data: {
-    
+    values:'',
   },
   //事件处理函数
   onLoad: function (option) {
-   
+
   },
-  btns: function(){
-    wx.switchTab({
-      url: '../index/index',
+
+  formSubmit: function(e){
+    var accessTokens = app.globalData.accessToken;
+    if (e.detail.value.input==''){
+      wx.showToast({
+        title: '验证失败',
+        icon: 'loading',
+        duration: 2000
+      })
+    } else if (e.detail.value.input == accessTokens){
+      wx.showToast({
+        title: '用户验证中...',
+        icon: 'loading',
+        duration: 2000
+      })
+      setTimeout(()=>{
+        wx.switchTab({
+          url: '../index/index',
+        })
+      },2000)
+    }else{
+      wx.showToast({
+        title: '登录失败',
+        icon: 'none',
+        duration: 2000
+      })
+    }
+  },
+  //一键复制
+  oneCopy(){
+    wx.setClipboardData({
+      data: 'd2b42e8f-0207-4bd5-a3f1-fa8d1c37e4d6',
+      success: function (res) {
+        wx.getClipboardData({
+          success: function (res) {
+            wx.showToast({
+              title: '复制成功',
+              icon: 'none',
+              duration: 2000
+            })
+          }
+        })
+      }
     })
-  },
+  }
+  
 
 })
