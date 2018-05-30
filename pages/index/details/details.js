7746fd93dd20261af131e588da175cb8d031e25b
sky-xsk <2530485432@qq.com>
2018-05-30 15:52:10 +0800

    增加评论功能

--- pages/index/details/details.js
+++ pages/index/details/details.js
@@ -6,10 +6,12 @@ var WxParse = require('../../../wxParse/wxParse.js');
 Page({
   data: {
     itemDetails: {},
+    itemId:'',
   },
   //事件处理函数
   onLoad: function (option) {
     var itemId = option.id;
+    this.data.itemId = itemId;
     var urls = "https://cnodejs.org/api/v1/topic/" + itemId;
     this.getTopicdetails(urls);
   },
@@ -32,6 +34,38 @@ Page({
 
     })
   },
+  //新建评论
+  replays(urls){
+    var _this = this;
+    wx.request({
+      url: urls,
+      data: {
+        accesstoken: "d2b42e8f-0207-4bd5-a3f1-fa8d1c37e4d6",
+        content:"着实的厉害" 
+      },
+      method: 'POST',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res)
+        wx.showToast({
+          title: '评论成功',
+          icon: 'success',
+          duration: 2000
+        }) 
+      }
+
+    })
+  },
+
+  //评论
+  relayTest(){
+    var urlsRepay = "https://cnodejs.org/api/v1/topic/" + this.data.itemId + "/replies";
+    this.replays(urlsRepay);
+   
+  },
+
  //查用户详情
   findUserDetails(event){
     var usernames = event.currentTarget.dataset.username;
